d9617fcb606f69a456864825b96cc435b0314d27
dung11122005 <dungakaishi900@gmail.com>
2024-04-25 00:55:26 +0700

    1

--- src/controllers/homecontrollers.js
+++ src/controllers/homecontrollers.js
@@ -36,7 +36,7 @@ let postwebhook = (req, res) => {
 
 
 let getwebhook = (req, res) => {
-    let PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+    let VERYFY_TOKEN = process.env.VERYFY_TOKEN;
     // Parse the query params
     let mode = req.query["hub.mode"];
     let token = req.query["hub.verify_token"];
@@ -45,7 +45,7 @@ let getwebhook = (req, res) => {
     // Check if a token and mode is in the query string of the request
     if (mode && token) {
         // Check the mode and token sent is correct
-        if (mode === "subscribe" && token === PAGE_ACCESS_TOKEN) {
+        if (mode === "subscribe" && token === VERYFY_TOKEN) {
             // Respond with the challenge token from the request
             console.log("WEBHOOK_VERIFIED");
             res.status(200).send(challenge);
--- src/server.js
+++ src/server.js
@@ -4,15 +4,15 @@ import viewengine from "./configs/viewengine";
 import webroutes from "./routes/web";
 
 let app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 //config view engine 
 viewengine(app);
 
 //config routes
 webroutes(app);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+
 let port = process.env.PORT || 8080;
 
 app.listen(port, () => {
