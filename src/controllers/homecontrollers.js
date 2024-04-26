10ef76c522da4d9c51bef6882017b3feba2462ab
dung11122005 <dungakaishi900@gmail.com>
2024-04-26 08:15:49 +0700

    1

--- src/controllers/homecontrollers.js
+++ src/controllers/homecontrollers.js
@@ -138,7 +138,7 @@ function callSendAPI(sender_psid, response) {
     // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
         "method": "POST",
         "json": request_body
     }, (err, res, body) => {
@@ -160,7 +160,7 @@ let setupprofile = (req, res) => {
     }
     // Send the HTTP request to the Messenger Platform
     request({
-        "uri": `https://graph.facebook.com/v18.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`,
+        "uri": `https://graph.facebook.com/v19.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`,
         "qs": { "access_token": PAGE_ACCESS_TOKEN },
         "method": "POST",
         "json": request_body
