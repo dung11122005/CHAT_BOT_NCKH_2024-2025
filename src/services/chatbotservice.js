f0f8457b9909da8febe1cda4ed83e2b60da2c2c7
dung11122005 <dungakaishi900@gmail.com>
2024-04-26 10:16:09 +0700

    1

---
+++ src/services/chatbotservice.js
@@ -0,0 +1,41 @@
+import request from "request";
+require('dotenv').config();
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+let callSendAPI = (sender_psid, response) => {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v19.0/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+let hendlegetstarted = (sender_psid) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let response = { "text": "Oops, try sending another image." }
+            await callSendAPI(sender_psid, response)
+            resolve('done')
+        } catch (e) {
+
+        }
+    })
+}
+module.exports = {
+    hendlegetstarted: hendlegetstarted
+
+}
