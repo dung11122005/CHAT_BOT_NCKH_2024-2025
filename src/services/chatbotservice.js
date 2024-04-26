7df7508b55c165495a0ad6731bd64b8960412252
dung11122005 <dungakaishi900@gmail.com>
2024-04-27 06:58:28 +0700

    1

--- src/services/chatbotservice.js
+++ src/services/chatbotservice.js
@@ -2,6 +2,7 @@ import { response } from "express";
 import request from "request";
 require('dotenv').config();
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const IMAGE_GET_STARTED = 'https://lavenderstudio.com.vn/wp-content/uploads/2017/03/chup-san-pham.jpg'
 let callSendAPI = (sender_psid, response) => {
     // Construct the message body
     let request_body = {
@@ -49,14 +50,67 @@ let hendlegetstarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
             let username = await getusername(sender_psid);
-            let response = { "text": `XIN CHÀO ${username} NHÉ, TỚ LÀ DŨNG RẤT VUI ĐƯỢC TRÒ CHUYỆN VỚI CẬU` }
-            await callSendAPI(sender_psid, response)
+            let response1 = { "text": `XIN CHÀO ${username} NHÉ, TỚ LÀ DŨNG RẤT VUI ĐƯỢC TRÒ CHUYỆN VỚI CẬU` }
+            let response2 = sendgetstartedtemplate();
+            // send teck masseges
+            await callSendAPI(sender_psid, response1)
+
+            //send generic template messenge
+            await callSendAPI(sender_psid, response2)
+
             resolve('done')
         } catch (e) {
             reject(e)
         }
     })
 }
+
+let sendgetstartedtemplate = () => {
+    let response = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [
+                    {
+                        "title": "XIN CHÀO BẠN ĐẾN VỚI NHÀ HÀNG CỦA DŨNG!",
+                        "image_url": IMAGE_GET_STARTED,
+                        "subtitle": "DƯỚI ĐÂY LÀ ACCS LỰA CHỌN CỦA NHÀ HÀNG",
+                        "default_action": {
+                            "type": "web_url",
+                            "url": "https://petersfancybrownhats.com/view?item=103",
+                            "messenger_extensions": false,
+                            "webview_height_ratio": "tall",
+                            "fallback_url": "https://petersfancybrownhats.com/"
+                        },
+                        "buttons": [
+                            {
+                                "type": "web_url",
+                                "url": "https://petersfancybrownhats.com",
+                                "title": "View Website"
+                            }, {
+                                "type": "postback",
+                                "title": "MENU CHÍNH",
+                                "payload": "MAIN_MENU"
+                            }, {
+                                "type": "postback",
+                                "title": "ĐẶT BÀN",
+                                "payload": "RESERVE_TABLE"
+                            }, {
+                                "type": "postback",
+                                "title": "HƯỚNG DẪN SỬ DỤNG CHAT",
+                                "payload": "GUIDE_TO_USE"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+    return response
+}
+
+
 module.exports = {
     hendlegetstarted: hendlegetstarted
 
