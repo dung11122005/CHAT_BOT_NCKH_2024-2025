729692d8625b02e5a3bf4cbb1cd8a2cd6011f8ca
dung11122005 <dungakaishi900@gmail.com>
2024-04-26 17:01:16 +0700

    1

--- src/services/chatbotservice.js
+++ src/services/chatbotservice.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import request from "request";
 require('dotenv').config();
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
@@ -23,6 +24,26 @@ let callSendAPI = (sender_psid, response) => {
             console.error("Unable to send message:" + err);
         }
     });
+}
+
+let getusername = async (sender_psid) => {
+    let username = '';
+    // Send the HTTP request to the Messenger Platform
+    await request({
+        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        console.log(body)
+        if (!err) {
+            let response = JSON.parse(res)
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+    return username
 }
 let hendlegetstarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
