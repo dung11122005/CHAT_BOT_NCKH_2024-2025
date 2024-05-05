3004e731c0409890276df6ab625854757ddd138b
dung11122005 <dungakaishi900@gmail.com>
2024-05-05 09:21:02 +0700

    1

--- src/controllers/homecontrollers.js
+++ src/controllers/homecontrollers.js
@@ -85,6 +85,11 @@ function handleMessage(sender_psid, received_message) {
         // Create the payload for a basic text message, which
         // will be added to the body of our request to the Send API
         response = {
+            // client.message(received_message)
+            //     .then(data => {
+            //         console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
+            //     })
+            //     .catch(console.error);
             "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
         }
     } else if (received_message.attachments) {
